9adf9bc73fbba47f92e72429c7de8512ed00f21a
geo_varenik <varenik_geo@mail.ru>
2020-08-23 15:28:00 +0300

    set up login and signup

--- accounts/views.py
+++ accounts/views.py
@@ -4,4 +4,6 @@ from django.views.generic import CreateView
 from . import forms
 
 class SignUp(CreateView):
-    pass
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login') #lazy перенаправит только после заполнения форм, простой reverse - сразу 
+    template_name = 'accounts/signup.html' #accounts - это название приложения!
