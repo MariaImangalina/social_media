e7838693ba825b0c638bf6e722d13c7669263b9e
geo_varenik <varenik_geo@mail.ru>
2020-08-24 16:25:27 +0300

    fill out group templates

--- groups/views.py
+++ groups/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
+from django.urls import reverse
+from groups.models import Group, GroupMember
 
-# Create your views here.
+class CreateGroupView(generic.CreateView, LoginRequiredMixin):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
--- posts/models.py
+++ posts/models.py
@@ -25,7 +25,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('posts:single', kwargs={'username':self.user.username,
                                                 'pk':self.pk})
-
     class Meta:
         ordering = ['-created_at']
         unique_together = ['user', 'message']
