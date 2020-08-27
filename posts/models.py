43ec057c54d4fde2f287144aa849b41b19b82401
geo_varenik <varenik_geo@mail.ru>
2020-08-27 18:45:08 +0300

    set up templates in posts

--- posts/models.py
+++ posts/models.py
@@ -9,7 +9,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Post(models.Model):
-    user = models.ForeignKey(User, related_name = 'post', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name = 'posts', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now=True)
     message = models.TextField()
     message_html = models.TextField(editable = False)
